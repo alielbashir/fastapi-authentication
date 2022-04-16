3e12174eca9331d0158c3118973622dfd360867e
Ali Elbashir <alielbashiir@gmail.com>
2022-04-16 06:01:10 +0300

    Implement routes

--- src/main.py
+++ src/main.py
@@ -1,18 +1,39 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+
+from .auth import AuthHandler
+from .schema import AuthDetails
 
 app = FastAPI()
 
+auth_handler = AuthHandler()
 users = []
 
 
-@app.post("/register")
-def register():
-    return {}
+@app.post("/register", status_code=201)
+def register(auth_details: AuthDetails):
+    if any(x["username"] == auth_details.username for x in users):
+        # if name is taken
+        raise HTTPException(status_code=400, detail="Username is taken")
+    hashed_pass = auth_handler.get_password_hash(auth_details.password)
+    users.append({"username": auth_details.username, "password": hashed_pass})
+    return
 
 
 @app.post("/login")
-def login():
-    return {}
+def login(auth_details: AuthDetails):
+    user = None
+    for x in users:
+        if x["username"] == auth_details.username:
+            user = x
+            break
+
+    if user is None or not auth_handler.verify_password(
+        auth_details.password, user["password"]
+    ):
+        raise HTTPException(status_code=401, detail="Invalid username and/or password")
+    token = auth_handler.encode_token(user["username"])
+
+    return {"token": token}
 
 
 @app.post("/unprotected")
@@ -21,5 +42,5 @@ def unprotected():
 
 
 @app.post("/protected")
-def protected():
-    return {}
+def protected(username=Depends(auth_handler.auth_wrapper)):
+    return {"name": username}
